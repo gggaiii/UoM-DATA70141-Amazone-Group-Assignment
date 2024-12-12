91ed7a076f9e9ca340f9108b1ff98b91d385cf87
mikeuom <ducp78767@gmail.com>
2024-12-12 05:48:37 +0000

    An update from Mike
    Committed the script for creating collections and synthetic data in json.files

---
+++ collections/create_collections.py
@@ -0,0 +1,89 @@
+import json
+import pandas as pd
+import pprint
+import os
+from pymongo import MongoClient
+from bson import ObjectId
+from datetime import datetime
+
+
+# Connect to the server:
+con ="mongodb+srv://admin:admin@amazone.dodun.mongodb.net/"
+client = MongoClient(con, serverSelectionTimeoutMS=5000)
+try:
+    print("Connected to the Server")
+except Exception:
+    print("Unable to connect to the server.")
+
+db = client['Amazone']
+
+#Reset the database
+collections = {
+    "warehouses": "warehouses.json",
+    "products": "products.json",
+    "freshProducts": "freshProducts.json",
+    "others": "others.json",
+    "books": "books.json",
+    "cds": "cds.json",
+    "mobilePhones": "mobilePhones.json",
+    "homeAppliances": "homeAppliances.json",
+    "dailyInventory": "dailyInventory.json",
+    "stores": "stores.json",
+    "customerAddresses": "customerAddresses.json",
+    "partners": "partners.json",
+    "customers": "customers.json",
+    "customerRatings": "customerRatings.json",
+    "pastOrders": "pastOrders.json",
+    "pastOrderItems": "pastOrderItems.json"
+}
+
+for collection in collections.keys():
+    db[collection].drop()
+
+
+
+#Load the data
+
+def convert_ids_to_objectid(data):
+    if isinstance(data, list):
+        return [convert_ids_to_objectid(item) for item in data]
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            if key.endswith("ID") or key.endswith("id"):
+                try:
+                    data[key] = ObjectId(value)
+                except Exception:
+                    pass
+            else:
+                data[key] = convert_ids_to_objectid(value)
+        return data
+    else:
+        return data
+
+def convert_dates_to_isodate(data):
+    if isinstance(data, list):
+        return [convert_dates_to_isodate(item) for item in data]
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            if key.lower().endswith("date"):
+                try:
+                    data[key] = datetime.fromisoformat(value.replace("Z", ""))
+                except Exception:
+                    pass
+            else:
+                data[key] = convert_dates_to_isodate(value)
+        return data
+    else:
+        return data
+    
+for collection, file in collections.items():
+    file = open(f"./collections/{file}")
+    data = json.load(file)
+    data = convert_ids_to_objectid(data)
+    data = convert_dates_to_isodate(data)
+    if data:
+        db[f"{collection}"].insert_many(data)
+
+print("Data has been loaded successfully")
+
+    
